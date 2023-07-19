9aff88c7cdb79de98904c9dd1047b103af80ca5a
jinyoung9703 <wlsdudajtwu@gmail.com>
2023-07-19 18:06:53 +0900

    jinyoung- 칵테일 페이지_1

---
+++ src/main/java/hanjan/yeji/boot/woorisul/controller/CocController.java
@@ -0,0 +1,24 @@
+package hanjan.yeji.boot.woorisul.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/coc")
+public class CocController {
+
+    Logger logger = LogManager.getLogger(CocController.class);
+
+    @GetMapping("/list")
+    public String list(){
+        logger.info("coc/list 호출!!");
+
+        return "coc/list";
+    }
+
+
+}
