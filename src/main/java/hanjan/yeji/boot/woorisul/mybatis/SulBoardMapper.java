aab4758a3946176622c6fa2d478f4b1fd275d9ce
Moonsun23 <socoooool90@gmail.com>
2023-07-20 19:19:15 +0900

    우리술 찾기_DB입력 및 단위테스트

--- src/main/java/hanjan/yeji/boot/woorisul/mybatis/SulBoardMapper.java
+++ src/main/java/hanjan/yeji/boot/woorisul/mybatis/SulBoardMapper.java
@@ -3,8 +3,14 @@ package hanjan.yeji.boot.woorisul.mybatis;
 import hanjan.yeji.boot.woorisul.model.SulBoard;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface SulBoardMapper {
 
   int insertSulBoard(SulBoard sb);
+
+  List<SulBoard> selectSulBoard(int stnum);
+
+  int selectCountSulBoard();
 }
