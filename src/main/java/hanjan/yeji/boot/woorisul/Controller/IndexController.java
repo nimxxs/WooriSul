1b4a69b09dacee83bd81fc1a156d6f2361580c3f
bigdata <tnals2199@naver.com>
2023-07-24 18:03:37 +0900

    header : 기본틀 완성 intro : 기본틀 완성

--- src/main/java/hanjan/yeji/boot/woorisul/Controller/IndexController.java
+++ src/main/java/hanjan/yeji/boot/woorisul/Controller/IndexController.java
@@ -1,14 +1,23 @@
 package hanjan.yeji.boot.woorisul.Controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-
+    Logger logger = LogManager.getLogger(IndexController.class);
     @GetMapping("/")
     public String index() {
+        logger.info("index 호출!!");
 
         return "index";
+    }
+    @GetMapping("/intro")
+    public String intro() {
+        logger.info("intro 호출!!");
+
+        return "intro";
     }
 }
